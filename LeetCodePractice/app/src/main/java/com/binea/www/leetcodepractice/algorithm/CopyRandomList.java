4bba129ece4abdc86829d43c500ed0ba455ad924
xubinggui <xubinggui@zepplabs.com>
2016-05-14 00:45:29 +0800

    Add CopyRandomList

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/CopyRandomList.java
@@ -0,0 +1,94 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+/**
+ * Created by xubinggui on 5/14/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class CopyRandomList {
+
+    /**
+     * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+     * Return a deep copy of the list.
+     * Definition for singly-linked list with a random pointer.
+     * class RandomListNode {
+     * int label;
+     * RandomListNode next, random;
+     * RandomListNode(int x) { this.label = x; }
+     * };
+     */
+
+    class RandomListNode {
+        public int label;
+        public RandomListNode next, random;
+
+        public RandomListNode(int x) {
+            this.label = x;
+        }
+    }
+
+    public RandomListNode copyRandomList(RandomListNode head) {
+        RandomListNode iter = head, next;
+
+        // First round: make copy of each node,
+        // and link them together side-by-side in a single list.
+        while (iter != null) {
+            next = iter.next;
+
+            RandomListNode copy = new RandomListNode(iter.label);
+            iter.next = copy;
+            copy.next = next;
+
+            iter = next;
+        }
+
+        // Second round: assign random pointers for the copy nodes.
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+
+        // Third round: restore the original list, and extract the copy list.
+        iter = head;
+        RandomListNode pseudoHead = new RandomListNode(0);
+        RandomListNode copy, copyIter = pseudoHead;
+
+        while (iter != null) {
+            next = iter.next.next;
+
+            // extract the copy
+            copy = iter.next;
+            copyIter.next = copy;
+            copyIter = copy;
+
+            // restore the original list
+            iter.next = next;
+
+            iter = next;
+        }
+
+        return pseudoHead.next;
+    }
+}
