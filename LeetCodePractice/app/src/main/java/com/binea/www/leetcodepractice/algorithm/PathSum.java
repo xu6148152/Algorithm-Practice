8f104395d2d4a4b38bf3f69f51c24da3d2783023
Xu Binggui <xubinggui@zepplabs.com>
2016-06-10 14:55:46 +0800

    Add PathSum

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/PathSum.java
@@ -0,0 +1,66 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+/**
+ * Created by xubinggui on 6/10/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class PathSum {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that
+     * adding up all the values along the path equals the given sum.
+     *
+     * For example:
+     * Given the below binary tree and sum = 22,
+     * 5
+     * / \
+     * 4   8
+     * /   / \
+     * 11  13  4
+     * /  \      \
+     * 7    2      1
+     * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+     */
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        sum -= root.val;
+
+        if (root.left == null && root.right == null) {
+            return sum == 0;
+        }
+
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+}
