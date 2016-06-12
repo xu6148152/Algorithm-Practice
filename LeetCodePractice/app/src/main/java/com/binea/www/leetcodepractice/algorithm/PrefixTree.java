58eba8ef579763406dbc6e388eee569f91599801
Xu Binggui <xubinggui@zepplabs.com>
2016-06-13 00:34:35 +0800

    Add PrefixTree

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/PrefixTree.java
@@ -0,0 +1,86 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+/**
+ * Created by xubinggui on 6/13/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class PrefixTree {
+
+    /**
+     * Implement a trie with insert, search, and startsWith methods.
+     */
+    class TrieNode {
+        public boolean isWord;
+        public TrieNode[] children = new TrieNode[26];
+
+        public TrieNode() {
+        }
+    }
+
+    private TrieNode root;
+
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode ws = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (ws.children[c - 'a'] == null) {
+                ws.children[c - 'a'] = new TrieNode();
+            }
+            ws = ws.children[c - 'a'];
+        }
+        ws.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode ws = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (ws.children[c - 'a'] == null) {
+                return false;
+            }
+            ws = ws.children[c - 'a'];
+        }
+        return ws.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode ws = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (ws.children[c - 'a'] == null) {
+                return false;
+            }
+            ws = ws.children[c - 'a'];
+        }
+        return true;
+    }
+
+    // Your Trie object will be instantiated and called as such:
+    // Trie trie = new Trie();
+    // trie.insert("somestring");
+    // trie.search("key");
+}
