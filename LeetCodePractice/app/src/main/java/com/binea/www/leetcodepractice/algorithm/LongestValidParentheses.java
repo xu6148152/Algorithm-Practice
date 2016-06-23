55427eb2764f087dc4c07d4963e48eb0cc834460
Xu Binggui <xubinggui@zepplabs.com>
2016-06-23 10:55:33 +0800

    Update LongestValidParentheses

--- LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/LongestValidParentheses.java
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/LongestValidParentheses.java
@@ -1,5 +1,7 @@
 package com.binea.www.leetcodepractice.algorithm;
 
+import java.util.Stack;
+
 /**
  * Created by xubinggui on 6/23/16.
  * //                            _ooOoo_
@@ -25,4 +27,55 @@ package com.binea.www.leetcodepractice.algorithm;
  * //                  佛祖镇楼                  BUG辟易
  */
 public class LongestValidParentheses {
+
+    /**
+     * Given a string containing just the characters '(' and ')', find the length of the longest
+     * valid (well-formed) parentheses substring.
+     *
+     * For "(()", the longest valid parentheses substring is "()", which has length = 2.
+     *
+     * Another example is ")()())", where the longest valid parentheses substring is "()()", which
+     * has length = 4.
+     */
+    public int longestValidParentheses11ms(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        int left = -1;
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == '(') {
+                stack.push(j);
+            } else {
+                if (stack.isEmpty()) {
+                    left = j;
+                } else {
+                    stack.pop();
+                    if (stack.isEmpty()) {
+                        max = Math.max(max, j - left);
+                    } else {
+                        max = Math.max(max, j - stack.peek());
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+    int longestValidParentheses6ms(String s) {
+        if (s.length() <= 1) {
+            return 0;
+        }
+        int curMax = 0;
+        int[] longest = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')'
+                    && i - longest[i - 1] - 1 >= 0
+                    && s.charAt(i - longest[i - 1] - 1) == '(') {
+                longest[i] =
+                        longest[i - 1] + 2 + ((i - longest[i - 1] - 2 >= 0) ? longest[i - longest[i
+                                - 1] - 2] : 0);
+                curMax = Math.max(longest[i], curMax);
+            }
+        }
+        return curMax;
+    }
 }
