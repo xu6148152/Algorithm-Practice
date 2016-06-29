0e8e55d6be11c0bd21a7246dc2dd363ef44428f4
Xu Binggui <xubinggui@zepplabs.com>
2016-06-29 10:53:09 +0800

    Add SymmetricTree

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/SymmetricTree.java
@@ -0,0 +1,101 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+import java.util.Stack;
+
+/**
+ * Created by xubinggui on 6/29/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class SymmetricTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric1ms(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) {
+            return true;
+        }
+        if (leftNode == null || rightNode == null) {
+            return false;
+        }
+        return leftNode.val == rightNode.val
+                && isMirror(leftNode.left, rightNode.right)
+                && isMirror(leftNode.right, rightNode.left);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null)  return true;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode left, right;
+        if(root.left!=null){
+            if(root.right==null) return false;
+            stack.push(root.left);
+            stack.push(root.right);
+        }
+        else if(root.right!=null){
+            return false;
+        }
+
+        while(!stack.empty()){
+            if(stack.size()%2!=0)   return false;
+            right = stack.pop();
+            left = stack.pop();
+            if(right.val!=left.val) return false;
+
+            if(left.left!=null){
+                if(right.right==null)   return false;
+                stack.push(left.left);
+                stack.push(right.right);
+            }
+            else if(right.right!=null){
+                return false;
+            }
+
+            if(left.right!=null){
+                if(right.left==null)   return false;
+                stack.push(left.right);
+                stack.push(right.left);
+            }
+            else if(right.left!=null){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
