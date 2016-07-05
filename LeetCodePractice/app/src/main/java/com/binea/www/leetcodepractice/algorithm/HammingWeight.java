74d749c8dd4c4ccdb0cfe1df9a849bba953123f5
Xu Binggui <xubinggui@zepplabs.com>
2016-07-05 12:03:55 +0800

    Add HammingWeight

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/HammingWeight.java
@@ -0,0 +1,44 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+/**
+ * Created by xubinggui on 7/5/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class HammingWeight {
+
+    /**
+     * Write a function that takes an unsigned integer and returns the number of ’1' bits it has
+     * (also known as the Hamming weight).
+     *
+     * For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011,
+     * so the function should return 3.
+     */
+    public static int hammingWeight1ms1(int n) {
+        int ones = 0;
+        while (n != 0) {
+            ones = ones + (n & 1);
+            n = n >>> 1;
+        }
+        return ones;
+    }
+}
