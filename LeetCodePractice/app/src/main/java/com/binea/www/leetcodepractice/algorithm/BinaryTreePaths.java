1a0bfb5fcde323804b2a8a8b7bce84939833c221
Xu Binggui <xubinggui@zepplabs.com>
2016-07-10 16:12:40 +0800

    Add BinaryTreePaths

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/BinaryTreePaths.java
@@ -0,0 +1,57 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xubinggui on 7/10/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class BinaryTreePaths {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public List<String> binaryTreePaths3ms(TreeNode root) {
+        List<String> answer = new ArrayList<String>();
+        if (root != null) {
+            searchBT(root, "", answer);
+        }
+        return answer;
+    }
+
+    private void searchBT(TreeNode root, String path, List<String> answer) {
+        if (root.left == null && root.right == null) {
+            answer.add(path + root.val);
+        }
+        if (root.left != null) {
+            searchBT(root.left, path + root.val + "->", answer);
+        }
+        if (root.right != null) {
+            searchBT(root.right, path + root.val + "->", answer);
+        }
+    }
+}
