4f352d79de84cc155d0e6e9e4142a3cac6eba817
Xu Binggui <xubinggui@zepplabs.com>
2016-07-13 10:58:22 +0800

    Add RecoverTree

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/RecoverTree.java
@@ -0,0 +1,88 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+/**
+ * Created by xubinggui on 7/13/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class RecoverTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    TreeNode firstElement = null;
+    TreeNode secondElement = null;
+    // The reason for this initialization is to avoid null pointer exception in the first comparison when prevElement has not been initialized
+    TreeNode prevElement = new TreeNode(Integer.MIN_VALUE);
+
+    /**
+     * Morris Traversal
+     * Two elements of a binary search tree (BST) are swapped by mistake.
+     *
+     * Recover the tree without changing its structure.
+     *
+     * Note:
+     * A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+     */
+    public void recoverTree4ms(TreeNode root) {
+
+        // In order traversal to find the two elements
+        traverse(root);
+
+        // Swap the values of the two nodes
+        int temp = firstElement.val;
+        firstElement.val = secondElement.val;
+        secondElement.val = temp;
+    }
+
+    private void traverse(TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+
+        traverse(root.left);
+
+        // Start of "do some business",
+        // If first element has not been found, assign it to prevElement (refer to 6 in the example above)
+        if (firstElement == null && prevElement.val >= root.val) {
+            firstElement = prevElement;
+        }
+
+        // If first element is found, assign the second element to the root (refer to 2 in the example above)
+        if (firstElement != null && prevElement.val >= root.val) {
+            secondElement = root;
+        }
+        prevElement = root;
+
+        // End of "do some business"
+
+        traverse(root.right);
+    }
+}
