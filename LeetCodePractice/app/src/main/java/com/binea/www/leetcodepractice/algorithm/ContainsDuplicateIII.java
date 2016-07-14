108465d78e1745f8131ac132d030f2dfe8434e87
Xu Binggui <xubinggui@zepplabs.com>
2016-07-14 10:51:04 +0800

    Add ContainsDuplicateIII

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/ContainsDuplicateIII.java
@@ -0,0 +1,54 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by xubinggui on 7/14/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class ContainsDuplicateIII {
+
+    /**
+     * Given an array of integers, find out whether there are two distinct indices i and j in the array such that the difference between nums[i] and
+     * nums[j] is at most t and the difference between i and j is at most k.
+     */
+    public boolean containsNearbyAlmostDuplicate21ms(int[] nums, int k, int t) {
+        if (k < 1 || t < 0) return false;
+        Map<Long, Long> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
+            long bucket = remappedNum / ((long) t + 1);
+            if (map.containsKey(bucket)
+                    || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)
+                    || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t))
+                return true;
+            if (map.entrySet().size() >= k) {
+                long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+                map.remove(lastBucket);
+            }
+            map.put(bucket, remappedNum);
+        }
+        return false;
+    }
+}
