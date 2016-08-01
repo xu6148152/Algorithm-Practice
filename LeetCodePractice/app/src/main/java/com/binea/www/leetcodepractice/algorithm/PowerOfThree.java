3c3cc1b32d4266cf5a6add2e1215abe923c70ff8
Xu Binggui <xubinggui@zepplabs.com>
2016-08-01 10:54:01 +0800

    Add PowerOfThree

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/PowerOfThree.java
@@ -0,0 +1,35 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+//  Created by xubinggui on 8/1/16.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+public class PowerOfThree {
+    /**
+     * Given an integer, write a function to determine if it is a power of three.
+     *
+     * Follow up:
+     * Could you do it without using any loop / recursion?
+     */
+    public boolean isPowerOfThree(int n) {
+        return (n > 0 && 1162261467 % n == 0);
+    }
+}
