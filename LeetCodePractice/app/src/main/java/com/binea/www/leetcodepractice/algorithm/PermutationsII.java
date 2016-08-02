56044e85f39f66675ddfa01c20f226b7a29b6d47
Xu Binggui <xubinggui@zepplabs.com>
2016-08-02 11:02:18 +0800

    Add PermutationsII

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/PermutationsII.java
@@ -0,0 +1,73 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//  Created by xubinggui on 8/2/16.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+public class PermutationsII {
+
+    /**
+     * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+     *
+     * For example,
+     * [1,1,2] have the following unique permutations:
+     * [
+     * [1,1,2],
+     * [1,2,1],
+     * [2,1,1]
+     * ]
+     */
+    public List<List<Integer>> permuteUnique4ms(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        boolean[] used = new boolean[nums.length];
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        dfs(nums, used, list, res);
+        return res;
+    }
+
+    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) {
+                continue;
+            }
+            used[i] = true;
+            list.add(nums[i]);
+            dfs(nums, used, list, res);
+            used[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
+}
