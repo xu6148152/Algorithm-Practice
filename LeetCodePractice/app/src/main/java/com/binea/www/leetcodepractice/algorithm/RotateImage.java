2ffa4baaa9fb66e0a9f63bb5890278e8317433f6
Xu Binggui <xubinggui@zepplabs.com>
2016-08-03 11:04:15 +0800

    Add RotateImage

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/RotateImage.java
@@ -0,0 +1,52 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+//  Created by xubinggui on 8/3/16.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+public class RotateImage {
+    /**
+     * You are given an n x n 2D matrix representing an image.
+     *
+     * Rotate the image by 90 degrees (clockwise).
+     *
+     * Follow up:
+     * Could you do this in-place?
+     */
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i =0 ; i<matrix.length; i++){
+            for(int j = 0; j<matrix.length/2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+}
