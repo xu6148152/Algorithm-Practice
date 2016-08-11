f9b50d63276a57db5fa5b96a39e83b294f7c1e7a
Xu Binggui <xubinggui@zepplabs.com>
2016-08-11 10:59:00 +0800

    ExcelSheetColumnNumber

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/ExcelSheetColumnNumber.java
@@ -0,0 +1,43 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+//  Created by xubinggui on 8/11/16.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+public class ExcelSheetColumnNumber {
+
+    /**
+     * Given a column title as appear in an Excel sheet, return its corresponding column number.
+     *
+     * For example:
+     *
+     * A -> 1
+     * B -> 2
+     * C -> 3
+     * ...
+     * Z -> 26
+     * AA -> 27
+     * AB -> 28
+     */
+    public int titleToNumber(String s) {
+        return 0;
+    }
+}
