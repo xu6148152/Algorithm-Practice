b273895a1560d12e4afcae7158f51daf9af7b9b0
Xu Binggui <xubinggui@zepplabs.com>
2016-08-11 11:17:10 +0800

    Add ExcelSheetColumnNumber

--- LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/ExcelSheetColumnNumber.java
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/ExcelSheetColumnNumber.java
@@ -38,6 +38,12 @@ public class ExcelSheetColumnNumber {
      * AB -> 28
      */
     public int titleToNumber(String s) {
-        return 0;
+        int result  = 0;
+        for (int i = 0; i < s.length(); i++){
+            result *= 26;
+            result += ((s.charAt(i) - 'A') + 1);
+        }
+
+        return result;
     }
 }
