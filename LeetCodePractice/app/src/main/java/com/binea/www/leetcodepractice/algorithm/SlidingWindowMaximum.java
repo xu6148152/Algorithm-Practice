d16a306aad0d00dc583d087d08e8bd9be9cba82b
xubinggui <xubinggui@zepplabs.com>
2016-08-23 11:08:50 +0800

    Update SlidingWindowMaximum

--- LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/SlidingWindowMaximum.java
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/SlidingWindowMaximum.java
@@ -1,5 +1,8 @@
 package com.binea.www.leetcodepractice.algorithm;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //  Created by xubinggui on 8/22/16.
 //                            _ooOoo_  
 //                           o8888888o  
@@ -45,7 +48,30 @@ public class SlidingWindowMaximum {
      * Note:
      * You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
      */
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        return null;
+    public int[] maxSlidingWindow(int[] a, int k) {
+        if (a == null || k <= 0) {
+            return new int[0];
+        }
+        int n = a.length;
+        int[] r = new int[n-k+1];
+        int ri = 0;
+        // store index
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < a.length; i++) {
+            // remove numbers out of range k
+            while (!q.isEmpty() && q.peek() < i - k + 1) {
+                q.poll();
+            }
+            // remove smaller numbers in k range as they are useless
+            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+                q.pollLast();
+            }
+            // q contains index... r contains content
+            q.offer(i);
+            if (i >= k - 1) {
+                r[ri++] = a[q.peek()];
+            }
+        }
+        return r;
     }
 }
