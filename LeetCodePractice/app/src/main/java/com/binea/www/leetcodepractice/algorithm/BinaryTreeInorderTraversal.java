22166896ba6c84dddc743a737bd88b9d3a3637c5
xubinggui <xubinggui@zepplabs.com>
2016-08-31 11:01:45 +0800

    Add BinaryTreeInorderTraversal

---
+++ LeetCodePractice/app/src/main/java/com/binea/www/leetcodepractice/algorithm/BinaryTreeInorderTraversal.java
@@ -0,0 +1,70 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+//  Created by xubinggui on 8/31/16.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+public class BinaryTreeInorderTraversal {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * Given a binary tree, return the inorder traversal of its nodes' values.
+     *
+     * For example:
+     * Given binary tree [1,null,2,3],
+     * 1
+     * \
+     * 2
+     * /
+     * 3
+     * return [1,3,2].
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+
+        while (cur != null || !stack.empty()) {
+            while (cur != null) {
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+
+        return list;
+    }
+}
