76bfbf06a8eb7a07f63534080341319f4eeccdb9
Binea <xu6148152@gmail.com>
2016-10-20 23:29:50 +0800

    Add LongestSubsequence

---
+++ introduction2algorithm/src/main/java/com/example/dp/LongestSubsequence.java
@@ -0,0 +1,48 @@
+package com.example.dp;
+
+/**
+ * Created by binea on 2016/10/20.
+ */
+
+public class LongestSubsequence {
+    public int[][] getLongestSubsequence(int[] x, int y[]) {
+
+        int m = x.length;
+        int n = y.length;
+
+        int[][] b = new int[m][m];
+        int[][] c = new int[m][n];
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; i <n; j++) {
+                if(x[i] == y[j]) {
+                    c[i][j] = c[i - 1][j - 1] + 1;
+                    b[i][j] = 1;
+                }else if(c[i - 1][j] >= c[i][j - 1]) {
+                    c[i][j] = c[i - 1][j];
+                    b[i][j] = 0;
+                }else {
+                    c[i][j] = c[i][j - 1];
+                    b[i][j] = -1;
+                }
+            }
+        }
+
+        return b;
+    }
+
+    public void display(int[][]b, int[] x, int i, int j) {
+        if(i == 0 || j == 0) {
+            return;
+        }
+
+        if(b[i][j] == 1) {
+            display(b, x, i - 1, j - 1);
+            System.out.print(x[i] + " ");
+        }else if(b[i][j] == 0) {
+            display(b, x, i - 1, j);
+        }else {
+            display(b, x, i, j - 1);
+        }
+    }
+}
--- introduction2algorithm/src/test/java/UnitTest.java
+++ introduction2algorithm/src/test/java/UnitTest.java
@@ -1,4 +1,5 @@
 import com.example.dp.Fab;
+import com.example.dp.LongestSubsequence;
 
 import org.junit.Test;
 
@@ -12,5 +13,14 @@ public class UnitTest {
     @Test
     public void testFab() {
         System.out.print(Fab.getFab(40));
+    }
+
+    @Test
+    public void testLongestSubsequence() {
+        LongestSubsequence longestSubsequence = new LongestSubsequence();
+        int[] a = {1, 0, 0, 1, 0, 1, 0, 1};
+        int[] b = {0, 1, 0, 1, 1, 0, 1, 1, 0};
+        int[][] result = longestSubsequence.getLongestSubsequence(a, b);
+        longestSubsequence.display(result, a, a.length, b.length);
     }
 }
