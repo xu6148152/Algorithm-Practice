f580bc627dc9955b04f4422eb92dabf8de1ae85c
xubinggui <xubinggui@zepplabs.com>
2016-11-04 10:44:03 +0800

    Add SingleNumberII

---
+++ leetcode/src/main/java/com/binea/www/leetcodepractice/algorithm/SingleNumberII.java
@@ -0,0 +1,42 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+//  Created by xubinggui on 04/11/2016.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+
+public class SingleNumberII {
+
+    /**
+     * Given an array of integers, every element appears three times except for one. Find that single one.
+     *
+     * Note:
+     * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+     */
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ones = nums[i] ^ ones & ~twos;
+            twos = twos ^ nums[i] & ~ones;
+        }
+        return ones;
+    }
+}
