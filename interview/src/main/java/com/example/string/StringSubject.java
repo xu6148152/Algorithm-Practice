2eb2f6b63ffee8e0de874642b4aa60b52e83c00a
xubinggui <xubinggui@zepplabs.com>
2016-11-05 17:14:19 +0800

    Add rotate

--- interview/src/main/java/com/example/string/StringSubject.java
+++ interview/src/main/java/com/example/string/StringSubject.java
@@ -185,4 +185,29 @@ public class StringSubject {
         size += 2;
         return size;
     }
+
+    public void rotate(int[][] matrix, int n) {
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+
+                int top = matrix[first][i];
+
+                //left to top
+                matrix[first][i] = matrix[last - offset][first];
+
+                //bottom to right
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                //right to bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                //top to right
+                matrix[i][last] = top;
+            }
+        }
+    }
 }
