a344bd8532032e854a2c9d5c37922439628733b0
Binea <xu6148152@gmail.com>
2016-11-08 23:49:00 +0800

    Add StackTest

---
+++ interview/src/main/java/com/example/stack_queue/Stack.java
@@ -0,0 +1,61 @@
+package com.example.stack_queue;
+
+/**
+ * Created by binea on 2016/11/8.
+ */
+
+public class Stack {
+    public class Node {
+        public int val;
+        public Node next;
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private Node top;
+
+    private Node min;
+
+    private Node root;
+
+    private int capacity = 0;
+
+    public void push(Node node) {
+        if(node == null) {
+            return;
+        }
+
+
+        if(root != null) {
+            top.next = node;
+        }else {
+            root = top = node;
+        }
+
+        if(node.val <= top.val) {
+            min = node;
+        }
+
+        top = node;
+
+        capacity++;
+    }
+
+    public Node pop() {
+        Node node = root;
+        Node topNode = new Node(top.val);
+        while(node != null && node.next != top) {
+            node = node.next;
+        }
+        if(node != null) {
+            top = node;
+            node.next = null;
+        }
+        return topNode;
+    }
+
+    public Node min() {
+        return min;
+    }
+}
---
+++ interview/src/test/java/Stack_QueueTest.java
@@ -0,0 +1,24 @@
+import com.example.stack_queue.Stack;
+
+import org.junit.Test;
+
+/**
+ * Created by binea on 2016/11/8.
+ */
+
+public class Stack_QueueTest {
+    @Test
+    public void testStack1() {
+        Stack stack = new Stack();
+        stack.push(new Stack(). new Node(1));
+        stack.push(new Stack(). new Node(2));
+        stack.push(new Stack(). new Node(3));
+
+
+//        System.out.println(stack.min().val);
+        System.out.println(stack.pop().val);
+        System.out.println(stack.pop().val);
+        System.out.println(stack.pop().val);
+    }
+
+}
