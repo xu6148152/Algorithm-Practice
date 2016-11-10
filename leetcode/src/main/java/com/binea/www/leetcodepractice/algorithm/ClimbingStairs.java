2cf55923a03bcbab32293c894788515f30dfc781
xubinggui <xubinggui@zepplabs.com>
2016-11-10 11:26:15 +0800

    Add ClimblingStairs

---
+++ leetcode/src/main/java/com/binea/www/leetcodepractice/algorithm/ClimbingStairs.java
@@ -0,0 +1,59 @@
+package com.binea.www.leetcodepractice.algorithm;
+
+//  Created by xubinggui on 10/11/2016.
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易 
+
+public class ClimbingStairs {
+
+    /**
+     * You are climbing a stair case. It takes n steps to reach to the top.
+     *
+     * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+     */
+    public int climbStairs(int n) {
+        int a = 1, b = 1, c = 0;
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+    public int climbStairsWithDP(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[n] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
