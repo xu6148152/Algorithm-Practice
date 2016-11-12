ffc457c1d8118e9456a8c9b4a31a6b4d95cf7a97
Binea <xu6148152@gmail.com>
2016-11-13 01:12:25 +0800

    Add getPre getNext

--- interview/src/main/java/com/example/bits/Bits.java
+++ interview/src/main/java/com/example/bits/Bits.java
@@ -42,4 +42,97 @@ public class Bits {
         }
         return sb.toString();
     }
+
+    public int getNext(int n) {
+        int c = n;
+        int c0 = 0;
+        int c1 = 0;
+        while (((c & 1) == 0) && (c != 0)) {
+            c0++;
+            c >>= 1;
+        }
+
+        while ((c & 1) == 1) {
+            c1++;
+            c >>= 1;
+        }
+
+        if (c0 + c1 == 31 || c0 + c1 == 0) {
+            return -1;
+        }
+
+        int p = c0 + c1;
+        n |= (1 << p);
+        n &= ~((1 << p) - 1);
+        n |= (1 << (c1 - 1)) - 1;
+
+        return n;
+    }
+
+    public int getPrev(int n) {
+        int temp = n;
+        int c0 = 0;
+        int c1 = 0;
+        while ((temp & 1) == 1) {
+            c1++;
+            temp >>= 1;
+        }
+
+        if (temp == 0) {
+            return -1;
+        }
+
+        while (((temp & 1) == 0) && (temp != 0)) {
+            c0++;
+            temp >>= 1;
+        }
+
+        int p = c0 + c1;
+        n *= ((~0) << (p + 1));
+
+        int mask = (1 << (c1 + 1)) - 1;
+        n |= mask << (c0 - 1);
+        return n;
+    }
+
+    public int getNextArith(int n) {
+        int c = n;
+        int c0 = 0;
+        int c1 = 0;
+        while (((c & 1) == 0) && (c != 0)) {
+            c0++;
+            c >>= 1;
+        }
+
+        while ((c & 1) == 1) {
+            c1++;
+            c >>= 1;
+        }
+
+        if (c0 + c1 == 31 || c0 + c1 == 0) {
+            return -1;
+        }
+        return n + (1 << c0) + (1 << (c1 - 1)) - 1;
+    }
+
+    public int getPreArith(int n) {
+        int temp = n;
+        int c0 = 0;
+        int c1 = 0;
+        while ((temp & 1) == 1) {
+            c1++;
+            temp >>= 1;
+        }
+
+        if (temp == 0) {
+            return -1;
+        }
+
+        while (((temp & 1) == 0) && (temp != 0)) {
+            c0++;
+            temp >>= 1;
+        }
+
+        return n - (1 << c1) - (1 << (c0 - 1)) + 1;
+    }
 }
