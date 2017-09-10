3e152e48f6a94bb1319f35e8605eebd04e22f8f7
xu6148152 <xu6148152@gmail.com>
2017-09-10 22:55:39 +0800

    Add RemoveElement

---
+++ LeetCode/src/main/java/com/binea/www/leetcodepractice/algorithm/easy/RemoveElement.java
@@ -0,0 +1,39 @@
+package com.binea.www.leetcodepractice.algorithm.easy;
+
+/**
+ * Created by binea on 10/9/2017.
+ */
+
+public class RemoveElement {
+    /**
+     * Given an array and a value, remove all instances of that value in place and return the new length.
+     * <p>
+     * Do not allocate extra space for another array, you must do this in place with constant memory.
+     * <p>
+     * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+     * <p>
+     * Example:
+     * Given input array nums = [3,2,2,3], val = 3
+     * <p>
+     * Your function should return length = 2, with the first two elements of nums being 2.
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j + 1;
+    }
+}
